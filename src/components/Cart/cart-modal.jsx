949e83c03fa68a7db396c132d28f5418261ddd4c
mwitumi21@gmail.com <git>
2021-01-04 12:16:43 +0800

    Created Cart Item component and linked it with the store

--- src/components/Cart/cart-modal.jsx
+++ src/components/Cart/cart-modal.jsx
@@ -1,16 +1,32 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
 import CustomButton from '../CustomButton/CustomButton'
+import CartItem from './cart-item'
 
 import './cart-modal.scss'
 
 
-const CartModal =() => {
+
+const CartModal = ({ cartItems }) => {
     return (
         <div className='cart-dropdown'>
-            <div className='cart-items'/>
-            <CustomButton> CHECKOUT</CustomButton>
+            <div className='cart-items'>
+                {
+                    cartItems.map(cartItem => (
+                    <CartItem key={cartItem.id} item={cartItem} />
+                ))}
+            </div>
+            <CustomButton>GO TO CHECKOUT</CustomButton>
         </div>
+
     )
 }
 
-export default CartModal
+const mapStateToProps = (state) => ({
+    cartItems: state.cart.cartItems
+
+})
+
+
+export default connect(mapStateToProps)(CartModal)
