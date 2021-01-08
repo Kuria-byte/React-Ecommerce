7b7ec8b9707ea65644bb2ba1f6adef5bdaba9402
mwitumi21@gmail.com <git>
2021-01-08 13:03:37 +0800

    Implement Redux-persist, Moved our directory state to Redux

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+
 
 //STYLE
 import './index.css';
 
 // STORE
-import store from './Redux/store';
+import { store, persitor } from './Redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-
+      <PersistGate persistor={persitor}>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </PersistGate>
     </BrowserRouter>
   </Provider>
   ,
